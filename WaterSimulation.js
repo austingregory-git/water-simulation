cb6d689cfa15573a644021a3eab37ec502b474ae
JoshFloorGuy <joshf98@aol.com>
2019-12-13 12:33:22 -0600

    added basic water mesh and "isUnderwater" test

--- WaterSimulation.js
+++ WaterSimulation.js
@@ -4,9 +4,11 @@ var texLoader = new THREE.TextureLoader();
 
 async function start()
 {
+	var waterWidth = 200;
+	var waterLength = 200;
 	var scene = new THREE.Scene();
 	var bgScene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera(45, 1.5, 0.1, 1000 );
+	var camera = new THREE.PerspectiveCamera(45, 1.5, 0.1, 10000 );
 	camera.position.x = 60;
 	camera.position.y = 40;
 	camera.position.z = 60;
@@ -34,17 +36,37 @@ async function start()
 	
 	bgScene.add(bgMesh);
 	
+	var waterUniforms = {
+		isUnderwater: {value: 0}
+	}
+	
+	var WaterMaterial = new THREE.ShaderMaterial( {
+		fragmentShader: document.getElementById("WaterFragment").textContent,
+		vertexShader: document.getElementById("WaterVertex").textContent,
+		side: THREE.DoubleSide,
+		uniforms: waterUniforms
+	});
+	
+	var water = new THREE.Mesh(new THREE.PlaneGeometry(waterWidth,waterLength,1),WaterMaterial);
+	water.position.y = 0;
+	water.rotateX(Math.PI/2);
+	
+	scene.add(water);
+	console.log(water);
+	
+	var underwater = false;
+	
 	var render = function() {
 		requestAnimationFrame( render );
 		// Renders skybox
 		bgMesh.position.copy(camera.position);
 		renderer.render(bgScene, camera);
 		
+		underwater = (camera.position.y < water.position.y) && (camera.position.x > water.position.x-waterWidth/2 && camera.position.x < water.position.x+waterWidth/2) && (camera.position.z > water.position.z-waterLength/2 && camera.position.z < water.position.z+waterLength/2);
+		waterUniforms.isUnderwater.value = (underwater) ? 1 : 0;
 		
 		
-		
-		
-		
+		console.log(underwater);
 		// Final Render
 		renderer.render(scene, camera);
 	}
