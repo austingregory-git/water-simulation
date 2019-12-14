8cb5cd674e8733f25e3b29b85288cab05f4688b3
agregory <agregory@iastate.edu>
2019-12-14 05:21:33 +0000

    Replace WaterSimulation.js

--- FinalProject/WaterSimulation.js
+++ FinalProject/WaterSimulation.js
@@ -22,6 +22,7 @@ async function start()
 	var underwater = false;
 	
 	var camera = new THREE.PerspectiveCamera(45, 1.5, 0.1, 1000 );
+	camera.rotation.order = "YZX";
 	camera.position.x = 60;
 	camera.position.y = 40;
 	camera.position.z = 60;
@@ -32,10 +33,16 @@ async function start()
 	var waterWidth = 200;
 	var waterLength = 200;
 	
+	var dudvMap = texLoader.load("images/waterDUDV.png");
+	console.log(dudvMap);
+	
 	var waterUniforms = {
 		isUnderwater: {value: 0},
 		reflection: { type: 't', value: reflectionBuffer.texture },
-		refraction: { type: 't', value: refractionBuffer.texture }
+		refraction: { type: 't', value: refractionBuffer.texture },
+		dudvMap: { type: 't', value: dudvMap },
+		hideWater: {value: 1},
+		waterOffset: {value: 1}
 	}
 	
 	var WaterMaterial = new THREE.ShaderMaterial( {
@@ -68,26 +75,58 @@ async function start()
 	
 	bgScene.add(bgMesh);
 	
+	var cube = new THREE.Mesh(new THREE.BoxGeometry(10,10,10), new THREE.MeshPhysicalMaterial( { color: 0x00ff00, emissive: 0x008800,roughness:0.80,metalness:.41}));
+	
+	cube.position.y = 20
+	
+	scene.add(cube);
+	
+	
+	var light = new THREE.HemisphereLight( 0x0000ff, 0x00ff00, 0.3 ); 
+	//scene.add(light);
+	light = new THREE.DirectionalLight( 0xffffff, 0.5 );
+	//light = new THREE.DirectionalLight( 0xffffff, 0.0 );
+	light.position.x = -40;
+	light.position.y = 30;
+	light.position.z = -20;
+	scene.add(light);
+	
+	console.log(sceneWithoutWater);
+	console.log(scene);
+	
+	var upDownPlanes = [new THREE.Plane( new THREE.Vector3( 0, 1, 0 ), -1 ),new THREE.Plane( new THREE.Vector3( 0, - 1, 0 ), 1 )];
+	
 	 var renderToBuffers = function() {
-		 var distance = 2 * (camera.position.y-water.position.y);
+		var reflectPlane = new THREE.Plane( new THREE.Vector3( 0, (underwater) ? -1 : 1, 0 ), (underwater) ? 1 : -1 );
+		var refractPlane = new THREE.Plane( new THREE.Vector3( 0, (underwater) ? 1 : -1, 0 ), (underwater) ? -1 : 1 );
+		
+		waterUniforms.hideWater.value=1;
+		var distance = 2 * (camera.position.y-water.position.y);
+		
 		//Switches to reflect
 		camera.position.y-=distance;
 		camera.rotation.x = -camera.rotation.x;
 		bgMesh.position.copy(camera.position);
-		renderer.setRenderTarget(refractionBuffer);
+		renderer.setRenderTarget(reflectionBuffer);
 		renderer.render(bgScene, camera);
-		renderer.render(sceneWithoutWater, camera);
+		renderer.clippingPlanes = [reflectPlane];
+		renderer.render(scene, camera);
 		
 		//Switches to refract
 		camera.position.y+=distance
 		camera.rotation.x = -camera.rotation.x;
 		bgMesh.position.copy(camera.position);
-		renderer.setRenderTarget(reflectionBuffer);
+		renderer.setRenderTarget(refractionBuffer);
 		renderer.render(bgScene, camera);
-		renderer.render(sceneWithoutWater, camera);
+		renderer.clippingPlanes = [refractPlane];
+		renderer.render(scene, camera);
 		
+		renderer.clippingPlanes = [new THREE.Plane( new THREE.Vector3( 0, 0, 0 ), 0 )];
 		renderer.setRenderTarget(null);
+		 waterUniforms.hideWater.value=0;
 	} 
+	
+	var clock = new THREE.Clock(true);
 	
 	var render = function() {
 		requestAnimationFrame( render );
@@ -95,8 +134,10 @@ async function start()
 		bgMesh.position.copy(camera.position);
 		renderer.render(bgScene, camera);
 		
-		underwater = (camera.position.y < water.position.y) && (camera.position.x > water.position.x-waterWidth/2 && camera.position.x < water.position.x+waterWidth/2) && (camera.position.z > water.position.z-waterLength/2 && camera.position.z < water.position.z+waterLength/2);
+		underwater = (camera.position.y < water.position.y) //&& (camera.position.x > water.position.x-waterWidth/2 && camera.position.x < water.position.x+waterWidth/2) && (camera.position.z > water.position.z-waterLength/2 && camera.position.z < water.position.z+waterLength/2);
 		waterUniforms.isUnderwater.value = (underwater) ? 1 : 0;
+		waterUniforms.waterOffset.value+=0.03*clock.getDelta();
+		waterUniforms.waterOffset.value%=1;
 		
 		renderToBuffers();
 		
